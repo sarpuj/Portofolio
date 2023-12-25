1424d1359a483dd67d6a87be8108a72be0753122
sarpuj <sarapujakesuma31@gmail.com>
2023-12-25 14:14:30 +0700

    Dark and Light Mode

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
 
-function openFullImg(pict){
-    fullImgBox.style.display = "flex";
-    fullImg.src = pict;
+function openFullImg(pict) {
+  fullImgBox.style.display = "flex";
+  fullImg.src = pict;
 }
 
-function closeFullImg(){
-    fullImgBox.style.display = "none";
+function closeFullImg() {
+  fullImgBox.style.display = "none";
 }
 
-function setActive(index) {
-    // Mengambil semua elemen navbar
-    var navbarItems = document.querySelectorAll('.navbar-nav a');
-  
-    // Menghapus kelas 'active' dari semua elemen navbar
-    navbarItems.forEach(function(item) {
-      item.classList.remove('active');
-    });
-  
-    // Menambahkan kelas 'active' ke elemen yang diklik
-    navbarItems[index].classList.add('active');
-  }
-  
+
+// Mengaktifkan menu pada navbar saat di klik
+// function setActive(index) {
+//   // Mengambil semua elemen navbar
+//   var navbarItems = document.querySelectorAll(".navbar-nav a");
+
+//   // Menghapus kelas 'active' dari semua elemen navbar
+//   navbarItems.forEach(function (item) {
+//     item.classList.remove("active");
+//   });
+
+//   // Menambahkan kelas 'active' ke elemen yang diklik
+//   navbarItems[index].classList.add("active");
+// }
+
+// Dark and Light Mode
+var icon = document.getElementById('icon');
+
+icon.onclick = function() {
+    document.body.classList.toggle('LightMode');
+    if(document.body.classList.contains('LightMode')) {
+        this.classList.toggle('bi-moon-fill');
+        this.classList.remove('bi-brightness-high-fill');
+    } else {
+        this.classList.toggle('bi-brightness-high-fill');
+        this.classList.remove('bi-moon-fill');
+    }
+}
